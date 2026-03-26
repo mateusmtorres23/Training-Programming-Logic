63ffedbab27a418216df6700489f0e94358704ee
Mateus Montalvão Torres <mmtorres230726@gmail.com>
2026-03-26 14:04:28 -0300

    Refactor square function to use grid function

--- exercises-haskell/list-2/ex03_list2.hs
+++ exercises-haskell/list-2/ex03_list2.hs
@@ -2,4 +2,4 @@ grid :: Int -> Int -> [(Int, Int)]
 grid a b = [(x, y) | x <- [0..a], y <- [0..b]]
 
 square :: Int -> [(Int, Int)]
-square a = [(x, y) | x <- [0..a], y <- [0..a], x /= y]
+square a = [(x, y) | (x, y) <- grid n n, x /= y]
